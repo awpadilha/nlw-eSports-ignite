89a61401b567d05b19a00220f2175a193b4308e8
awpadilha <awpadilha.dev@gmail.com>
2022-09-16 21:45:50 -0300

    Stage 3 - To be continued - Part 1/2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,24 @@
-import express  from 'express';
+import express, { response }  from 'express';
+import {PrismaClient} from '@prisma/client' 
 
 const app = express();
+const prisma = new PrismaClient()
 
-// localhost:3333/ads
+// HTTP methjods / API RESTful
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, respose) => {
+    return response.json([]);
+});
+
+app.post('/ads', (request, respose) => {
+    return response.json([201]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+
+    // const gameId = request.params.id
+
+
     return response.json([
         {id: 1, name : 'Anuncio 1'},
         {id: 2, name : 'Anuncio 2'},
@@ -12,6 +26,13 @@ app.get('/ads', (request, response) => {
         {id: 4, name : 'Anuncio 4'},
         {id: 5, name : 'Anuncio 5'},
     ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+
+    // const adId = request.params.id
+
+    return response.json([])
 })
 
 app.listen(3333)
